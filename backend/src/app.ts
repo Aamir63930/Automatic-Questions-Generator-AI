d0a898499638f754688c6dcd106d4e4a14ece648
Aamir63930 <akumarjaan123@gmail.com>
2026-07-07 23:54:32 +0530

    update cors for vercel frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,13 @@ import paperRoutes        from './routes/paper.routes'
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: '*', credentials: true }))
+app.use(cors({
+  origin: [
+    "https://aiqpg-frontend-3wh6.vercel.app",
+    "http://localhost:3000"
+  ],
+  credentials: true
+}))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
